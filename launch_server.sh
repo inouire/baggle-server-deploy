1baffb25d629df5205aa6798c52b5ca208526117
inouire <edouard@melix.net>
2012-11-22 00:01:55 +0100

    Adding launcher script

---
+++ launch_server.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+last_server_version="2.3"
+
+#move working directory to script path
+cd -P $(dirname $0)
+
+#if no arg: use default config
+if [ $# -eq 0 ]; then
+    echo "No argument specified, using default config file conf/server-default.xml"
+    config="-c conf/server-default.xml"
+fi
+
+#if one argument, use this config
+if [ $# -ge 1 ]; then
+    echo "Using config file $1"
+    config="-c $1"
+fi
+
+#build main jar name
+main_jar="bin/baggle-server-$last_server_version.jar"
+
+#launch server
+java -jar $main_jar $config
+
