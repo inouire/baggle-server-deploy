50e0cb7f4486f970a66d84461f241c22eac2822a
inouire <edouard@melix.net>
2013-07-18 23:45:57 +0200

    Update server launcher

--- launch_server.sh
+++ launch_server.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-
 #move working directory to script path
 cd -P $(dirname $0)
-
-#get last server version
-source SERVER_VERSION
 
 #if no arg: use default config
 if [ $# -eq 0 ]; then
     echo "No argument specified, using default config file conf/server-default.xml"
-    config="-c conf/server-default.xml"
+    config="-c conf/server-default.myml"
 fi
 
 #if one argument, use this config
@@ -19,9 +15,6 @@ if [ $# -ge 1 ]; then
     config="-c $1"
 fi
 
-#build main jar name
-main_jar="bin/baggle-server-$last_server_version.jar"
-
 #launch server
-java -jar $main_jar $config
+java -jar bin/baggle-server.jar $config
 
